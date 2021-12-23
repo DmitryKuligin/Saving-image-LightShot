aed6fdaa953e9c21d7a4af922a637073a16426f0
Дмитрий Кулигин <46721224+DmitryKuligin@users.noreply.github.com>
2021-12-23 20:26:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+import requests
+import random
+import string
+import argparse
+
+from bs4 import BeautifulSoup as BS
+from user_agent import generate_user_agent
+
+
+def string_generator(length):
+    while True:
+        chars_list = string.ascii_lowercase + string.digits
+        comb_list = random.choices(chars_list, k=length)
+        yield "".join(comb_list)
+
+
+def get_image_by_string(string):
+    url = "https://prnt.sc/" + string
+    headers = {"user-agent": generate_user_agent()}
+
+    response = requests.get(url, headers=headers)
+    soup = BS(response.text, "html.parser")
+
+    image_html = soup.find("img", class_="screenshot-image")
+
+    if image_html:
+        image_url = image_html.get("src")
+
+        if "http" in image_url:
+            return image_url
+
+
+def main():
+    """ Main entry point """
+
+    # set up argparse
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        "-n", "--count", type=int, default=1000,
+        help="number of images that will be scraped"
+    )
+    argparser.add_argument(
+        "-c", "--clear", action="store_true", default=False,
+        help="clear saved images"
+    )
+    args = argparser.parse_args()
+
+    # open file for writing
+    if args.clear:
+        file_mode = "w"
+    else:
+        file_mode = "a"
+
+    print("Target:", args.count)
+    print("Clear:", str(args.clear), end="\n")
+
+    f = open("images.html", file_mode)
+
+    # scraping
+    try:
+        counter = 0
+        str_gen = string_generator(6)
+
+        for some_str in str_gen:
+            image = get_image_by_string(some_str)
+
+            if image:
+                counter += 1
+
+                print(f"{counter}. image:\t{image}")
+
+                f.write(f'<img src="{image}" style="border: 3px solid red">')
+
+                if counter >= args.count:
+                    break
+
+    except KeyboardInterrupt:
+        print("\nExiting.")
+        exit()
+    finally:
+        f.close()
+
+
+if __name__ == "__main__":
+    main()
